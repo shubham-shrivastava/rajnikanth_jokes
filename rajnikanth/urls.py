fdff295d49019778c07373189eaba56c44ce8974
Shubham Shrivastava <Shubham Shrivastava>
2017-08-29 21:24:54 +0530

    gui

--- rajnikanth/urls.py
+++ rajnikanth/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from rajnikanth import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL,
-#                           document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
